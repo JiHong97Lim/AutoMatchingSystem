9673a666709aacc78859dc55748d7fa99d51328f
jihong.lim <jhlim2323@gmail.com>
2025-11-06 15:23:08 +0900

    front 배포 관련 파일 commit

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom/client";
-import { createBrowserRouter, Router, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
 import "./index.css";
