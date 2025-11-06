5f55e45491a07e5727ea916870e71ef0e475b617
jihong.lim <jhlim2323@gmail.com>
2025-11-06 15:05:52 +0900

    CORS 에러 수정

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+  origin: ['http://localhost:5173', 'https://oksu-matching-system.fly.dev', 'http://127.0.0.1:5173'],
+});
   const port = process.env.PORT ? Number(process.env.PORT) : 3000;
   // ★ Fly에서 확실히 잡히게 host를 명시
   await app.listen(port, '0.0.0.0');
